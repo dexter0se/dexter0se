0cf53b1afa7f0f5c3b76cf047d00354fbfff6f72
Varun Bhargava <vbhargava.iitr@gmail.com>
2021-06-05 01:00:07 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,10 +1,2 @@
-- 👋 Hi, I’m @dexter0se
-- 👀 I’m interested in bot
-- 🌱 I’m currently learning automation
-- 💞️ I’m looking to collaborate on everything
-- 📫 How to reach me dont lol
-
-<!---
-dexter0se/dexter0se is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
-You can click the Preview link to take a look at your changes.
---->
+# tip-collect
+Catched tip.cc tips
---
+++ bot/main.py
@@ -0,0 +1,40 @@
+import discum
+import time
+# import mysql.connector
+# from mysql.connector import Error
+# import pandas as pd 
+
+bot = discum.Client(token='ODQ4NjEzOTg3ODAwNDQ5MDQ0.YLPL8g.k52IulPTjwdNvdsfG0Wyew93IPo', log=False)
+
+bot.sendMessage("845616861981835337", "Damn, I was down. But it feels good to be back! :)")
+
+@bot.gateway.command
+def tipCatcher(resp):
+
+    if resp.event.ready_supplemental: #ready_supplemental is sent after ready
+
+        user = bot.gateway.session.user
+        print("Logged in as {}#{}".format(user['username'], user['discriminator']))
+
+    if resp.event.message:
+
+        msg = resp.parsed.auto()
+        
+        if (msg['author']['id'] == "617037497574359050") and ("title" in msg['embeds'][0].keys()):
+
+            if 'An airdrop appears' in msg['embeds'][0]['title']:
+
+                try:
+                    value = msg['embeds'][0]['description'].split('\xa0')[1].split(')')[0]
+                except:
+                    value = "$0"
+
+                reaction = msg['embeds'][0]['description'].split('\n\nReact with ')[1].split(' to collect it up before it disappears!')[0]
+
+                time.sleep(2)
+
+                if(float(value.split('$')[1]) >= 0.01) and (msg['channel_id'] != "617048119426678930"):
+
+                    bot.addReaction(msg['channel_id'],msg['id'],reaction)
+
+bot.gateway.run(auto_reconnect=True)
